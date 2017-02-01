54e6dc2c0d314c0499bba2503f26f4a5038fd789
mpi <mpi@deb2>
2017-02-01 14:38:33 -0500

    minimal changes, bashrc and rc.xml added

--- minimal_config/minimal_config.sh
+++ minimal_config/minimal_config.sh
@@ -6,6 +6,8 @@ curl -s -o ~/.bashrc $config_domain/.bashrc
 source ~/.bashrc
 
 curl -s -o ~/.config/openbox/rc.xml $config_domain/rc.xml
+# Arch has "rc.xml", debian "lxde-rc.xml"
+cp ~/.config/openbox/rc.xml ~/.config/openbox/lxde-rc.xml
 openbox --reconfigure
 
 curl -s -o ~/.vimrc $config_domain/.vimrc
